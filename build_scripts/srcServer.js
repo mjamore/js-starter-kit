2de0cccf288fe3d36472d0d4f886f1471d517555
ddcmichaela <michael.amore@coxautoinc.com>
2018-10-30 15:06:27 -0700

    Add HTTP requests

--- build_scripts/srcServer.js
+++ build_scripts/srcServer.js
@@ -2,7 +2,7 @@
 
 import express from 'express';
 import path from 'path';
-import open from 'open';
+import opn from 'opn';
 import webpack from 'webpack';
 import config from '../webpack.config.dev';
 
@@ -19,10 +19,18 @@ app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', (req, res) => {
+	res.json([
+		{"id": 1, "firstName": "Bob", "lastName": "Smith", "email": "bob@gmail.com"},
+		{"id": 2, "firstName": "Tammy", "lastName": "Norton", "email": "tnorton@yahoo.com"},
+		{"id": 3, "firstName": "Tina", "lastName": "Lee", "email": "lee.tina@hotmail.com"}
+	]);
+});
+
 app.listen(port, (err) => {
 	if(err) {
 		console.log(err);
 	} else {
-		open(`http://localhost:${port}`);
+		opn(`http://localhost:${port}`);
 	}
 });
